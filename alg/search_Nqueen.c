73aae0c07b4a1b7ba93073e5c732ee07efaca1fe
koo <jakeoung@gmail.com>
2024-10-03 21:00:19 +0900

    2024-10-03

---
+++ alg/search_Nqueen.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 4
+
+int row[N];
+
+int check(int r, int c) {
+    for (int i = 0; i < r; i++) {
+        if (row[i] == c || abs(row[i] - c) == abs(i - r)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void print_chess() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%c", j == row[i] ? 'Q' : '.');
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void search(int r) {
+    if (r == N) {
+        print_chess();
+        return;
+    }
+    for (int col = 0; col < N; col++) {
+        if (check(r, col)) {
+            row[r] = col;
+            search(r + 1);
+        }
+    }
+}
+
+int main() {
+    search(0);
+    return 0;
+}
